2410de95d49b0afb1c0680f1f54ee5dba10350dd
ravsil <rafaelvieiradas@gmail.com>
2025-09-16 19:02:51 -0300

    added users like the incas and aztecs
    Co-authored-by: helcioDuarte <sdfake12@gmail.com>

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,28 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+const users = [
+    { id: 1, name: 'Rafael' },
+    { id: 2, name: 'Helcio' },
+    { id: 3, name: 'Maria' },
+]
+// router.on('/').render('pages/home')
+
+router.get('/get-user/:id?', async ({ params }) => {
+    if (!params.id) {
+        return users;
+    } else {
+        const user = users.find(user => user.id === parseInt(params.id))
+        return user || { message: 'User not found' }
+    }
+})
+
+router.post('/create-user', async ({ request }) => {
+    const { name } = request.body()
+    users.push({
+        id: users.length + 1,
+        name: name || `User ${users.length + 1}`,
+    })
+
+    return users[users.length - 1]
+})
