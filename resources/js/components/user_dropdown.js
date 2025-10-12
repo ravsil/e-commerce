ca4158723014cfaf624330d77724223a2b337e25
helcioDuarte <sdfake12@gmail.com>
2025-10-11 22:04:14 -0300

    made a somewhat decent header

---
+++ resources/js/components/user_dropdown.js
@@ -0,0 +1,28 @@
+// resources/js/components/userDropdown.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownButton = document.getElementById('customDropdownButton')
+  const dropdownMenu = document.getElementById('customDropdownMenu')
+
+  if (dropdownButton && dropdownMenu) {
+    const toggleDropdown = () => {
+      dropdownMenu.classList.toggle('opacity-0')
+      dropdownMenu.classList.toggle('-translate-y-2')
+      dropdownMenu.classList.toggle('pointer-events-none')
+    }
+
+    dropdownButton.addEventListener('click', (event) => {
+      event.stopPropagation()
+      toggleDropdown()
+    })
+
+    window.addEventListener('click', function (event) {
+      // If the menu is open and the click is outside, close it
+      if (!dropdownMenu.classList.contains('opacity-0')) {
+        if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+          toggleDropdown()
+        }
+      }
+    })
+  }
+})
